9c91dfd8645e0ba627637a78b6c933d32bfcb818
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2023-10-20 20:59:55 -0400

    removed and readded customfile

---
+++ custom.ts
@@ -0,0 +1,19 @@
+/**
+ * Animation blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace customblinker {
+    /**
+     *this is a test extension
+     * x=x coordinate
+     y=ycoordinate
+     inerval=flash interval
+     */
+    //% block="blink x $x y $y every $interval ms"
+    export function blink(x: number, y: number, interval: number): void {
+        let sprite = game.createSprite(x, y)
+        sprite.setBlink(interval)
+        sprite.blink()
+
+    }
+}
